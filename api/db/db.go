a62d7af57037624c96f852b06ea44d6bcb0738e0
Vadim Alekseev <valekseev@ozon.ru>
2022-04-17 13:53:22 +0300

    chore: test github actions

--- api/db/db.go
+++ api/db/db.go
@@ -2,9 +2,14 @@ package db
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/sql-ressam/ressam/pg"
 )
+
+func init() {
+	fmt.Println("test github actions")
+}
 
 // PgExporter can get information about PostgreSQL schemas.
 type PgExporter interface {
